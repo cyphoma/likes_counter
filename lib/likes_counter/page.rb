2dd1bacc8230ee8cee8a1cf912160acf73330991
JoJoS <jojos003@free.fr>
2016-09-07 12:46:12 +0200

    Suppression du langage FQL pour passer par le graph api en version 2.7

--- lib/likes_counter/page.rb
+++ lib/likes_counter/page.rb
@@ -4,10 +4,6 @@ module LikesCounter
 
     attr_accessor :id, :name, :likes
 
-    def self.by_id(*ids)
-      Query.instance.pages_by_id(ids)
-    end
-
     def to_s
       "#{name} (#{id}): #{likes}"
     end
--- lib/likes_counter/query.rb
+++ lib/likes_counter/query.rb
@@ -1,4 +1,3 @@
-require "singleton"
 require "uri"
 require "open-uri"
 require "json"
@@ -6,17 +5,23 @@ require "json"
 module LikesCounter
 
   class Query
-    include Singleton
 
-    BASE_QUERY = "select page_id, name, fan_count from page where"
-    BASE_URL = "https://api.facebook.com/method/fql.query?query="
+    BASE_URL = "https://graph.facebook.com"
+    FIELDS = %w(id name fan_count)
     DEFAULT_FORMAT = "json"
 
+    def initialize(api_token, api_version = 'v2.7')
+      @api_token = api_token
+      @api_version = api_version
+    end
+
     def pages_by_id(*ids)
-      results = JSON.parse(open(construct_uri('page_id', ids)) { |io| io.read })
-      results.map do |entry|
+      results = JSON.parse(open(construct_uri('ids', ids)) { |io| io.read })
+      results.map do |result|
+        entry = result[1]
+
         page = Page.new
-        page.id = entry["page_id"]
+        page.id = entry["id"]
         page.name = entry["name"]
         page.likes = entry["fan_count"]
 
@@ -27,8 +32,8 @@ module LikesCounter
     private
 
     def construct_uri(filter, *values)
-      query = "#{BASE_QUERY} #{filter} IN (#{values.join(', ')})"
-      "#{BASE_URL}#{URI::encode(query)}&format=#{DEFAULT_FORMAT}"
+      query = "#{filter}=#{values.join(',')}&fields=#{FIELDS.join(',')}"
+      "#{BASE_URL}/#{@api_version}/?#{query}&format=#{DEFAULT_FORMAT}&access_token=#{@api_token}"
     end
 
   end
